e09da9652e0e0bb198e26d9875c47075c192ea68
Yuval Laufer <yuvalaufer@yahoo.com>
2026-08-31 20:52:09 +0300

    lastone

--- nav.js
+++ nav.js
@@ -69,10 +69,10 @@ document.addEventListener("DOMContentLoaded", function() {
             <a href="https://open.spotify.com/artist/5GxEhBJJmhGECHQvsh9LRz?si=5T_ZVxBCQxKdX3BoawZUxA" target="_blank" class="cta-button" style="background-color: #1DB954;">
                 <span style="margin-left: 10px; font-size: 1.2em;">🎧</span> ספוטיפיי
             </a>
-            <a href="https://www.facebook.com/elkabetska" target="_blank" class="cta-button" style="background-color: #3b5998;">
+            <a href="https://www.facebook.com/shiri.elkabets.1" target="_blank" class="cta-button" style="background-color: #3b5998;">
                 <img src="images/facebooklogo.png" alt="Facebook"> פייסבוק
             </a>
-            <a href="https://instagram.com/elkabetska" target="_blank" class="cta-button" style="background-color: #E1306C;">
+            <a href="https://www.instagram.com/shiri_elkabetska/" target="_blank" class="cta-button" style="background-color: #E1306C;">
                 <img src="images/instagramlogo.png" alt="Instagram"> אינסטגרם
             </a>
             <a href="https://www.tiktok.com/@shirielkabets" target="_blank" class="cta-button" style="background-color: #000000;">
